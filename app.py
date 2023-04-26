53c9b1c4fe16e71d7e68ff1d9e18c0218d61d743
andharsm <andhar.moenir@gmail.com>
2023-04-26 15:53:40 +0700

    Pertama

---
+++ app.py
@@ -0,0 +1,11 @@
+#from flask_ngrok import run_with_ngrok
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    #run_with_ngrok(app)
+    app.run()
