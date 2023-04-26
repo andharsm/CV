bfc2faeff04a1a103d3bb5898efa29693e2c43c3
andharsm <andhar.moenir@gmail.com>
2023-04-26 16:07:50 +0700

    Fix Ngrok

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#from flask_ngrok import run_with_ngrok
+from flask_ngrok import run_with_ngrok
 from flask import Flask, render_template
 app = Flask(__name__, template_folder='templates')
 
@@ -7,5 +7,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    #run_with_ngrok(app)
+    run_with_ngrok(app)
     app.run()
